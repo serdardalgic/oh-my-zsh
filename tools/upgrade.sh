805fb41c1991fbbe4b47845f64e0438ae1259855
Serdar Dalgıç <sd@serdardalgic.org>
2013-12-09 12:54:18 +0200

    detail for maintaining this oh-my-zsh edition, details in upgrade.sh

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,4 +1,3 @@
-
 # Use colors, but only if connected to a terminal, and that terminal
 # supports them.
 if which tput >/dev/null 2>&1; then
@@ -20,20 +19,36 @@ else
   NORMAL=""
 fi
 
-printf "${BLUE}%s${NORMAL}\n" "Updating Oh My Zsh"
+printf "${BLUE}%s${NORMAL}\n" "Upgrading Oh My Zsh - Serdardalgic Edition:"
 cd "$ZSH"
-if git pull --rebase --stat origin master
-then
-  printf '%s' "$GREEN"
-  printf '%s\n' '         __                                     __   '
-  printf '%s\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
-  printf '%s\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
-  printf '%s\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
-  printf '%s\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
-  printf '%s\n' '                        /____/                       '
-  printf "${BLUE}%s\n" "Hooray! Oh My Zsh has been updated and/or is at the current version."
-  printf "${BLUE}${BOLD}%s${NORMAL}\n" "To keep up on the latest news and updates, follow us on twitter: https://twitter.com/ohmyzsh"
-  printf "${BLUE}${BOLD}%s${NORMAL}\n" "Get your Oh My Zsh swag at:  http://shop.planetargon.com/"
-else
-  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
-fi
+
+# serdar@boulder:~/GITHUB/oh-my-zsh^master
+# % git remote -v
+#   origin  git@github.com:serdardalgic/oh-my-zsh.git (fetch)
+#   origin  git@github.com:serdardalgic/oh-my-zsh.git (push)
+#   upstream        https://github.com/robbyrussell/oh-my-zsh.git (fetch)
+#   upstream        https://github.com/robbyrussell/oh-my-zsh.git (push)
+#
+# % git branch
+# * master
+#   mymaster
+#
+# I sync upstream/master with origin/master
+# Development is made in origin/mymaster
+
+printf '%s' "$GREEN"
+printf '%s\n' '         __                                     __   '
+printf '%s\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+printf '%s\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+printf '%s\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+printf '%s\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+printf '%s\n' '                        /____/                       '
+printf "${BLUE}%s\n"
+printf 'git checkout master'
+printf 'git pull upstream master'
+printf 'git push origin master' ' or add --force'
+printf 'git checkout mymaster'
+printf 'git pull origin mymaster'
+printf 'git rebase master'
+printf 'git push origin mymaster' ' or add --force'
+printf "${BLUE}${BOLD}%s${NORMAL}\n" "To keep up on the latest news and updates, follow us on twitter: https://twitter.com/ohmyzsh"
