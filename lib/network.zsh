e720705b15bb65a120c28ff665e2b43c99f4c006
Serdar Dalgıç <sd@serdardalgic.org>
2015-07-22 17:26:43 +0200

    Fix the network lib functions such as int-ip and int-ips

--- lib/network.zsh
+++ lib/network.zsh
@@ -3,10 +3,10 @@ alias http='python -m SimpleHTTPServer'
 function int-ip { 
     if [ $# -eq 1 ]; 
     then
-        /sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}';
+        /sbin/ifconfig $1 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}';
     else
-        /sbin/ifconfig | grep -v 127.0.0.1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}';
+        /sbin/ifconfig | grep -v 127.0.0.1 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}';
     fi
 }
-function int-ips { /sbin/ifconfig |grep -B1 "inet addr" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 ": " $3 }'; }
+function int-ips { /sbin/ifconfig |grep -B1 "inet " |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }'; }
 function ext-ip () { lynx --dump http://ipecho.net/plain; }
